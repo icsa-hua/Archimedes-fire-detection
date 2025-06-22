f4f326389ec3b4aef7ab142a3f1ea338689c8916
konstantinosgyftodimos <kgyftodimos@anadelta.hua.gr>
2025-06-22 20:04:04 +0300

    Config file

---
+++ src/config.py
@@ -0,0 +1,19 @@
+'''
+Fire detection params
+'''
+min_area = 0.85
+max_solidity=0.85
+shape_change_threshold=0.85
+
+'''
+Visualization Colors
+'''
+BGR_RED = (0, 0, 255)  
+BGR_ORANGE = (69,0,255)  
+
+'''
+LLM params
+'''
+model =  'llava:7b'
+temperature = 1.0
+max_tokens = None
