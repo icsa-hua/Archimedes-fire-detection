518a14452caa73fe55c8c9340be15afe203e0757
kaoulis <kyriakoschiotis@gmail.com>
2025-06-23 09:59:48 +0300

    Refactor: Remove unused iqa-scorer script

--- src/utils/distortions.py
+++ src/utils/distortions.py
@@ -5,6 +5,8 @@ import random
 import ctypes
 from pypiqe import piqe
 
+# Sorbel mask for edge detection
+
 # Utility: Convert between PIL and OpenCV
 def cv2_to_pil(img): return Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 def pil_to_cv2(img): return cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
